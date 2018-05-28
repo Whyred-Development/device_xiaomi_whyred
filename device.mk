2db11153931800328b87192caf225d8822966eb5
Shekhawat2 <anandsingh215@yahoo.com>
2018-05-28 06:17:57 +0000

    correct bt hal

--- device.mk
+++ device.mk
@@ -120,8 +120,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl-qti \
-    android.hardware.bluetooth@1.0-service-qti \
+    android.hardware.bluetooth@1.0 \
     libbt-vendor \
 
 # Camera
