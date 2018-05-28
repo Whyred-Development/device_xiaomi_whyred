8d3c5fc742cfbed517eb8bf42dcb01f085706d33
Shekhawat2 <anandsingh215@yahoo.com>
2018-05-28 09:25:06 +0000

    enable live display

--- device.mk
+++ device.mk
@@ -288,12 +288,12 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service
 
 # LiveDisplay native
-#PRODUCT_PACKAGES += \
-#    vendor.lineage.livedisplay@1.0-service-sdm \
-#    vendor.lineage.livedisplay-V1.0-java
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
 
-#PRODUCT_BOOT_JARS += \
-#    vendor.lineage.livedisplay-V1.0-java
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
 
 # Media
 PRODUCT_COPY_FILES += \
