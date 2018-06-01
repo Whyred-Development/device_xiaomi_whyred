6b31ebe22d3260fcaf7caaf77576b2ae2bcd118c
merothh <merothh@gmail.com>
2018-06-01 19:11:47 +0000

    whryed: Don't set Build Fingerprint via BUILD_PROP_OVERRIDE

--- lineage.mk
+++ lineage.mk
@@ -35,7 +35,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/8.5.11:user/release-keys \
     PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 8.5.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
