c365a3c7a8ab6726631d768c37573ecbf3405066
Dyneteve <sound0020@gmail.com>
2018-06-08 13:27:58 -0400

    Revert "Removed LiveDisplay"
    This reverts commit 786913ac553492f2806ff747e1eadf7429624c29.

--- device.mk
+++ device.mk
@@ -294,6 +294,14 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
+
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
